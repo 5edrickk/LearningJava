e7fb1c2f12e40df9aef47da4e8ca7b06891af340
Sèdrick Zahn <sezahn@icloud.com>
2026-01-20 16:29:12 -0500

    Initial commit

---
+++ src/ConversionDegrees.java
@@ -0,0 +1,53 @@
+import java.util.Scanner;
+
+public class ConversionDegrees {
+    private String type;
+    private Double degrees;
+
+    private Double convertToFahrenheit(Double degrees){
+        return degrees * 9.0 / 5.0 + 32;
+    }
+
+    private Double convertToCelsius(Double degrees){
+        return ((degrees - 32) / (9.0 / 5.0));
+    }
+
+
+    public ConversionDegrees(String type, Double degrees){
+        this.type = type;
+        this.degrees = degrees;
+    }
+
+    public static void main(String[] args){
+        Scanner userInput = new Scanner(System.in); // To read user input
+        boolean invalidResponse;
+        Double degrees = null;
+        String degreeType = null;
+
+        do {
+            invalidResponse = false;
+
+            System.out.println("Enter a value of type Double and the degree type (Celsius or Fahrenheit)"); //Asks user input
+            degrees = userInput.nextDouble(); // Reads user input
+            degreeType = userInput.nextLine();
+
+            if(degrees < 0 || !(degreeType.equals("Celsius") || degreeType.equals("Fahrenheit"))){
+                invalidResponse = true;
+                System.out.println("Invalid response, please insert correct values");
+            }
+
+        }
+        while(invalidResponse);
+
+        ConversionDegrees conversion = new ConversionDegrees(degreeType, degrees);
+        System.out.println("Degrees : " + conversion.degrees + " Type:" + conversion.type);
+
+        if (conversion.type.equals("Celsius")){
+            System.out.println("The value " + conversion.degrees + " in Fahrenheit is: " + conversion.convertToFahrenheit(conversion.degrees));
+        }
+        else{
+            System.out.println("The value " + conversion.degrees + " in Celsius is: " + conversion.convertToCelsius(conversion.degrees));
+        }
+
+    }
+}
---
+++ src/MessageBienvenue.java
@@ -0,0 +1,5 @@
+public class MessageBienvenue {
+    public static void main(String[] args){
+        System.out.println("Bienvenue au monde de Java !");
+    }
+}
